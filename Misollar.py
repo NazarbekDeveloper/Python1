aa27e9b14235d6a7393a847d8d37ddab19e7d0f0
NazarbekDeveloper <bekzalatoy05@gmail.com>
2026-02-10 18:04:23 +0500

    Yangi misollar yechimlari qo'shildi

--- Misollar.py
+++ Misollar.py
@@ -19,10 +19,51 @@ d = x % 10
 print("d = ",d)
 '''
 # 3-misol
-
+# a)
+'''
 n = (int)(input("n = "))
 m = (int)(input("m = "))
 a = ((n % 2 == 0) and (m % 2 == 0)) or ((n % 2 != 0) and (m % 2 != 0))
 print("a) shart: ",a)
+'''
+# b)
+'''
+A = input("(true/false) a = ")
+a = A.lower() == "true"
+B = input("(ture/false) b = ")
+b = B.lower() == "true"
+if a != b:
+    print(True)
+else:
+    print(False)
+'''
+# c)
+'''
+A = input("(true/false) a = ")
+a = A.lower() == "true"
+B = input("(ture/false) b = ")
+b = B.lower() == "true"
+C = input("(ture/false) c = ")
+c = C.lower() == "true"
+count = (a == True) + (b == True) + (c == True)
+if count == 1:
+    print(True)
+else:
+    print(False)
+'''
 
-print("b) shart: ")
+# 4-misol
+
+a = 1.5
+x = float(input("x = "))
+if x < -3:
+    y = math.tan(x) + math.sqrt(math.log(abs(a - 3)))
+elif -3 <= x and x <= 3:
+    y = (a**2 - 3)**2 - math.sin(2*x)
+else:
+    y = (a + 3) - math.cos(math.pi * x)
+print("y = ",y)
+
+
+
+
