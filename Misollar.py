d0709942034e1f6c78489cf612a654095a8b4d4e
NazarbekDeveloper <bekzalatoy05@gmail.com>
2026-02-10 08:33:35 +0500

    Yangi misol qo'shildi

--- Misollar.py
+++ Misollar.py
@@ -1,4 +1,7 @@
 import math
+
+# 1-misol:
+'''
 x = 17.421
 y = 10.365 * 10**-3
 z = 0.828 * 10**5
@@ -7,3 +10,19 @@ maxraj = abs(x - y) * (math.sin(z)**2 + math.tan(z))
 f = surat / maxraj
 
 print("f = ",round(f,5))
+'''
+# 2-misol:
+'''
+x = (float)(input("x = "))
+x = (int)(x*10)
+d = x % 10
+print("d = ",d)
+'''
+# 3-misol
+
+n = (int)(input("n = "))
+m = (int)(input("m = "))
+a = ((n % 2 == 0) and (m % 2 == 0)) or ((n % 2 != 0) and (m % 2 != 0))
+print("a) shart: ",a)
+
+print("b) shart: ")
